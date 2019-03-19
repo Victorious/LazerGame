d79c1dd24413ad92c18b4cf73d8bdf8b1c338e86
Marcus Wilen <marcus.wilen@hotmail.com>
2019-03-19 17:10:01 +0100

    Adding Swing window

--- LazerGame/src/Main/Game.java
+++ LazerGame/src/Main/Game.java
@@ -1,10 +1,23 @@
 package Main;
 
-public class Game {
-
+public class Game implements Runnable {
+	
+	int height = 725, width = 725;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Game();
 	}
+	
+	// Constructor for Game class, implementing the running method
+	public Game() {
+		run();
+	}
+	
+	// Run method
+	@Override
+	public void run() {
+		new Window(height, width);	
+	}
+	
+	
 
 }
--- LazerGame/src/Main/Window.java
+++ LazerGame/src/Main/Window.java
@@ -1,5 +1,25 @@
 package Main;
 
+import javax.swing.JFrame;
+
 public class Window {
 
+	int height, width;
+	
+	JFrame frame;
+	
+	
+	public Window(int height, int width) {
+		this.height = height;
+		this.width = width;
+		
+		createWindow();
+	}
+	
+	public void createWindow() {
+		frame = new JFrame("Title");
+		frame.setSize(width, height);
+		frame.show();
+	}
+	
 }
