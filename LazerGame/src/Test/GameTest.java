eaadbe8437038ddc5772bb0d2b2490714a711333
Marcus Wilen <marcus.wilen@hotmail.com>
2019-03-19 16:06:54 +0100

    init

--- LazerGame/src/Test/GameTest.java
+++ LazerGame/src/Test/GameTest.java
@@ -1,5 +1,18 @@
 package Test;
 
+<<<<<<< HEAD
 public class GameTest {
+=======
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+>>>>>>> branch 'master' of https://github.com/Victorious/LazerGame
 
 }
