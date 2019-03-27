759097a3bd0b9a5ce70fd37915a9a9ee8825980a
a144756 <katerina.rajndl@consultant.volvo.com>
2019-03-27 18:25:02 +0100

    TEST

--- LazerGame/src/Test/TestPlayer.java
+++ LazerGame/src/Test/TestPlayer.java
@@ -10,6 +10,20 @@ public class TestPlayer {
 	int x = 20 ,y = 20;
 	@Test
 	public void testPlayerMoveUp() {
+		Player p = new Player(x, y, 10, 10);
+		p.moveUp(10);
+		int yActual = p.getY();
+		assertEquals(yActual, 10);
+		
+	}
+	
+	@Test
+	public void testPlayerMoveUp2() {
+		Player p = new Player(x, 40, 10, 10);
+		p.moveUp(10);
+		int yActual = p.getY();
+		assertEquals(yActual, 30);
+		
 	}
 	
 	@Test
