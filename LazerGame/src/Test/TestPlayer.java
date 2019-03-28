34c66acf5e17c1252b8ace32007f6c0fc96633fe
Marcus Wilen <marcus.wilen@hotmail.com>
2019-03-28 22:55:24 +0100

    updating TestPlayer

--- LazerGame/src/Test/TestPlayer.java
+++ LazerGame/src/Test/TestPlayer.java
@@ -2,15 +2,23 @@ package Test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import Main.Game;
 import Main.Player;
 
 public class TestPlayer {
 	int x = 20 ,y = 20;
+	Game game;
+	@Before
+	public void getClasses() {
+		game = new Game();
+	}
+	
 	@Test
 	public void testPlayerMoveUp() {
-		Player p = new Player(x, y, 10, 10);
+		Player p = new Player(x, y, 10, 10,game);
 		p.moveUp(10);
 		int yActual = p.getY();
 		assertEquals(yActual, 10);
@@ -19,7 +27,7 @@ public class TestPlayer {
 
 	@Test
 	public void testPlayerMoveDown() {
-		Player p = new Player(x, y, 10, 10);
+		Player p = new Player(x, y, 10, 10,game);
 		p.moveDown(10);
 		int yActual = p.getY();
 		assertEquals(yActual, 30);
@@ -27,7 +35,7 @@ public class TestPlayer {
 	
 	@Test
 	public void testPlayerMoveLeft() {
-		Player p = new Player(x, y, 10, 10);
+		Player p = new Player(x, y, 10, 10,game);
 		p.moveLeft(10);
 		int yActual = p.getX();
 		assertEquals(yActual, 10);
@@ -35,7 +43,7 @@ public class TestPlayer {
 	
 	@Test
 	public void testPlayerMoveRight() {
-		Player p = new Player(x, y, 10, 10);
+		Player p = new Player(x, y, 10, 10,game);
 		p.moveRight(10);
 		int yActual = p.getX();
 		assertEquals(yActual, 30);
