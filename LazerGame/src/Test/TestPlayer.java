3717103024e8b7b813ebc93651ea3580a9d9d606
a144756 <katerina.rajndl@consultant.volvo.com>
2019-03-31 13:59:18 +0200

    mock try

--- LazerGame/src/Test/TestPlayer.java
+++ LazerGame/src/Test/TestPlayer.java
@@ -2,19 +2,40 @@ package Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import Main.Game;
+import Main.LaserBlock;
 import Main.Player;
 
+@RunWith (MockitoJUnitRunner.class)
 public class TestPlayer {
 	int x = 20 ,y = 20;
-	Game game;
-	@Before
-	public void getClasses() {
-		game = new Game();
-	}
+    Player p;
+	
+	// in order to be able to test
+	// collision functions we need to create a mock
+	//we need to inject laserblock list into game 
+	// BUT we are doing smth wrong or
+	// missing smth because laserblock list size is still 0
+	// So this micking part is not working
+	
+	//inject mock into game object
+	@InjectMocks
+	Game game = new Game();
+	
+	//creating mock that should be injected
+	@Mock
+	ArrayList<LaserBlock> mockedlaserBlockList = new ArrayList<LaserBlock>();
+	LaserBlock lb = new LaserBlock(99, 98, 3, 4,game);
 	
 	@Test
 	public void testPlayerMoveUp() {
@@ -24,10 +45,9 @@ public class TestPlayer {
 		assertEquals(yActual, 10);
 	}
 	
-
 	@Test
 	public void testPlayerMoveDown() {
-		Player p = new Player(x, y, 10, 10,game);
+		p = new Player(x, y, 10, 10,game);
 		p.moveDown(10);
 		int yActual = p.getY();
 		assertEquals(yActual, 30);
@@ -35,7 +55,7 @@ public class TestPlayer {
 	
 	@Test
 	public void testPlayerMoveLeft() {
-		Player p = new Player(x, y, 10, 10,game);
+		p = new Player(x, y, 10, 10,game);
 		p.moveLeft(10);
 		int yActual = p.getX();
 		assertEquals(yActual, 10);
@@ -43,15 +63,18 @@ public class TestPlayer {
 	
 	@Test
 	public void testPlayerMoveRight() {
-		Player p = new Player(x, y, 10, 10,game);
+		p = new Player(x, y, 10, 10,game);
 		p.moveRight(10);
 		int yActual = p.getX();
 		assertEquals(yActual, 30);
 	}
 	
 	@Test
-	public void testPlayerCollisionWall() {
-		
+	public void testPlayerCollisionUp() {
+		p = new Player(100,100, 10, 10,game);
+		p.moveUp(10);
+		int yActual = p.getX();
+		assertEquals(yActual, 107);
 	}
 
 
